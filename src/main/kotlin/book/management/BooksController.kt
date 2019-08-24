b1bea1248f5c4f18a0378beff7e7b841caaee9fa
yshutaro <yshutaro@gmail.com>
2019-08-25 00:54:00 +0900

    thymeleafを利用した書籍一覧表示（ダミー）版

--- src/main/kotlin/book/management/BooksController.kt
+++ src/main/kotlin/book/management/BooksController.kt
@@ -2,17 +2,28 @@ package book.management
 
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
-//import io.micronaut.http.HttpStatus
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.HttpResponse
+import io.micronaut.views.*
 
 @Controller("/books")
 class BooksController {
 
+    @View("index")
     @Get("/")
-    fun index(): String {
+    fun index(): HttpResponse<Map<String, List<Book>>> {
         val books = listOf(
                 Book(1, "FACT FULLNESS", "ハンズ・ロスリング", "日経BP"),
                 Book(2, "三体", "劉 慈欣", "早川書房")
         )
-        return books.toString()
+        return HttpResponse.ok(mapOf("books" to books))
     }
+
+//    fun index(): String {
+//        val books = listOf(
+//                Book(1, "FACT FULLNESS", "ハンズ・ロスリング", "日経BP"),
+//                Book(2, "三体", "劉 慈欣", "早川書房")
+//        )
+//        return books.toString()
+//    }
 }
