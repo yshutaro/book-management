07ba29ed1c61d24f9c59aed04daea4437f63b827
yshutaro <yshutaro@gmail.com>
2019-08-25 01:25:44 +0900

    余計なコメント行を削除した。

--- src/main/kotlin/book/management/BooksController.kt
+++ src/main/kotlin/book/management/BooksController.kt
@@ -18,12 +18,4 @@ class BooksController {
         )
         return HttpResponse.ok(mapOf("books" to books))
     }
-
-//    fun index(): String {
-//        val books = listOf(
-//                Book(1, "FACT FULLNESS", "ハンズ・ロスリング", "日経BP"),
-//                Book(2, "三体", "劉 慈欣", "早川書房")
-//        )
-//        return books.toString()
-//    }
 }
