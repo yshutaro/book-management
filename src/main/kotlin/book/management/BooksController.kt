c17134f73ab7a867d02e1c23c5ae1d883bab117f
yshutaro <yshutaro@gmail.com>
2019-08-26 00:08:50 +0900

    新規登録画面表示のところだけ。formなどは未完成。

--- src/main/kotlin/book/management/BooksController.kt
+++ src/main/kotlin/book/management/BooksController.kt
@@ -4,7 +4,7 @@ import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.HttpResponse
-import io.micronaut.views.*
+import io.micronaut.views.View
 
 @Controller("/books")
 class BooksController(private val bookRepository: BookRepository) {
@@ -12,11 +12,16 @@ class BooksController(private val bookRepository: BookRepository) {
     @View("index")
     @Get("/")
     fun index(): HttpResponse<Map<String, List<Book>>> {
-//        val books = listOf(
-//                Book(1, "FACT FULLNESS", "ハンズ・ロスリング", "日経BP"),
-//                Book(2, "三体", "劉 慈欣", "早川書房")
-//        )
         val books = bookRepository.findAll()
         return HttpResponse.ok(mapOf("books" to books))
     }
+
+    @View("new")
+    @Get("new")
+    // メソッド名がnewだとうまく動かない
+    fun newbook(): HttpResponse<Map<String, BookCreateForm>> {
+        val form = BookCreateForm()
+        return HttpResponse.ok(mapOf("bookCreateForm" to form))
+    }
+
 }
