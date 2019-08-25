d7e82f48b6ffa1e147858519eb75f7fcfd55e798
yshutaro <yshutaro@gmail.com>
2019-08-26 04:37:55 +0900

    書籍登録で一旦動作する状態。

--- src/main/kotlin/book/management/BookCreateForm.kt
+++ src/main/kotlin/book/management/BookCreateForm.kt
@@ -1,7 +1,7 @@
 package book.management
 
 import javax.inject.Singleton
-import javax.validation.constraints.NotBlank
+import org.hibernate.validator.constraints.NotBlank
 import javax.validation.constraints.Size
 
 @Singleton
--- src/main/kotlin/book/management/BooksController.kt
+++ src/main/kotlin/book/management/BooksController.kt
@@ -2,8 +2,11 @@ package book.management
 
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Post
+import io.micronaut.http.annotation.Body
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.HttpResponse
+import io.micronaut.http.MediaType
 import io.micronaut.views.View
 
 @Controller("/books")
@@ -20,6 +23,18 @@ class BooksController(private val bookRepository: BookRepository) {
     @Get("new")
     // メソッド名がnewだとうまく動かない
     fun newbook(): HttpResponse<Map<String, BookCreateForm>> {
+        val form = BookCreateForm()
+        return HttpResponse.ok(mapOf("bookCreateForm" to form))
+    }
+
+    @Post("/", consumes = [MediaType.APPLICATION_FORM_URLENCODED])
+//    fun create(@Body form:BookCreateForm): HttpResponse<BookCreateForm> {
+    fun create(name: String, author: String, publisher: String): HttpResponse<Map<String, BookCreateForm>> {
+//        val name = requireNotNull(form.name)
+//        val author = requireNotNull(form.author)
+//        val publisher = requireNotNull(form.publisher)
+        bookRepository.create(name, author, publisher)
+//        return HttpResponse.created(form);
         val form = BookCreateForm()
         return HttpResponse.ok(mapOf("bookCreateForm" to form))
     }
