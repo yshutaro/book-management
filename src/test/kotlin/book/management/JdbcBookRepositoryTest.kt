ef61870fc90ce1e99ed6e8212c01ee3ac7285e25
yshutaro <yshutaro@gmail.com>
2019-09-02 18:55:09 +0900

    テストの雛形追加。

--- src/test/kotlin/book/management/JdbcBookRepositoryTest.kt
+++ src/test/kotlin/book/management/JdbcBookRepositoryTest.kt
@@ -1,4 +1,18 @@
 package book.management
 
-class JdbcBookRepositoryTest {
-}
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+
+class JdbcBookRepositoryTest : Spek({
+    describe("Bookリポジトリのテスト") {
+        beforeEachTest {
+        }
+
+        afterEachTest {
+        }
+
+        it("") {
+        }
+    }
+})
