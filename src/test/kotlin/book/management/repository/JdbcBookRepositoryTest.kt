f79f159810723ac8ce8603247e8d4692eddc713d
yshutaro <yshutaro@gmail.com>
2019-09-03 00:48:09 +0900

    リポジトリのテストを追加した。

--- src/test/kotlin/book/management/repository/JdbcBookRepositoryTest.kt
+++ src/test/kotlin/book/management/repository/JdbcBookRepositoryTest.kt
@@ -1,18 +1,28 @@
 package book.management.repository
 
+import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
+import javax.inject.Inject
 
-class JdbcBookRepositoryTest : Spek({
+class JdbcBookRepositoryTest(@Inject var sut: JdbcBookRepository) : Spek({
     describe("Bookリポジトリのテスト") {
+
         beforeEachTest {
         }
 
         afterEachTest {
         }
 
-        it("") {
+        it("何も登録していない状態のときfindAllは空のリストを返す") {
+            val got = sut.findAll()
+            assertThat(got).isEmpty()
+        }
+        it("createで登録したデータをfindByで取得する") {
+            val book = sut.create("testbook", "testauthor", "testpublisher")
+            val got = sut.findById(book.id)
+            assertThat(got).isEqualTo(book)
         }
     }
 })
