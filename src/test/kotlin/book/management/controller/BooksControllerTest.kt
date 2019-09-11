6d6c9a4293c552c6e7d64180141b325f38774897
yshutaro <yshutaro@gmail.com>
2019-09-11 10:23:46 +0900

    テストコードの書き方を下記ページに合わせた。NGになるはずのテストが通ってしまう。 https://guides.micronaut.io/creating-your-first-micronaut-app/guide/

--- src/test/kotlin/book/management/controller/BooksControllerTest.kt
+++ src/test/kotlin/book/management/controller/BooksControllerTest.kt
@@ -3,6 +3,7 @@ package book.management.controller
 import book.management.Book
 import book.management.BookForm
 import io.micronaut.context.ApplicationContext
+import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
 import io.micronaut.runtime.server.EmbeddedServer
@@ -14,48 +15,72 @@ import org.junit.jupiter.api.Assertions.assertNotNull
 import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.exceptions.HttpClientResponseException
 import org.assertj.core.api.Assertions.assertThat
+import io.micronaut.http.client.RxHttpClient
+import io.micronaut.http.client.annotation.Client
+import org.junit.jupiter.api.Test
+import javax.inject.Inject
+import io.micronaut.test.annotation.MicronautTest;
 
-object BooksControllerTest : Spek({
 
 
-    describe("/books") {
-        val embeddedServer : EmbeddedServer = ApplicationContext.run(EmbeddedServer::class.java)
-        val client : HttpClient = HttpClient.create(embeddedServer.url)
+//object BooksControllerTest : Spek({
+//
+//
+//    describe("/books") {
+//        val embeddedServer : EmbeddedServer = ApplicationContext.run(EmbeddedServer::class.java)
+//        val client : HttpClient = HttpClient.create(embeddedServer.url)
+//
+//        it ("top page") {
+//            val body = client.toBlocking().retrieve("/books")
+//            val books = listOf<Book>()
+//            val form = BookForm()
+//            val result =  HttpResponse.ok(mapOf("books" to books, "bookForm" to form))
+//            //TODO 正しくテスト出来ていない？
+////            val result =  HttpResponse.ok("aa") // これでも通ってしまう。
+//            assertEquals(result, body)
+//        }
+//
+//        it ("new page") {
+//            val body = client.toBlocking().retrieve("/books/new")
+//            val form = BookForm()
+//            val result:HttpResponse<*> =  HttpResponse.ok(mapOf("bookForm" to form))
+//            //TODO 正しくテスト出来ていない？
+//            assertEquals(result, body)
+//        }
+//
+//        it ("edit page") {
+//            val body = client.toBlocking().retrieve("/books/[0-9].*/edit")
+//            //TODO 正しくテスト出来ていない？
+//            assertThat(body).contains("書籍変更")
+//        }
+//
+//        it ("no page") {
+//            val body = client.toBlocking().retrieve("/hello")
+////            val body = client?.toBlocking()?.retrieve("/books")
+////            assertEquals("", body)
+//            assertNotNull(body)
+//        }
+//
+//        afterGroup {
+//            client.close()
+//            embeddedServer.close()
+//        }
+//    }
+//})
+@MicronautTest
+class BooksControllerTest {
 
-        it ("top page") {
-            val body = client.toBlocking().retrieve("/books")
-            val books = listOf<Book>()
-            val form = BookForm()
-            val result =  HttpResponse.ok(mapOf("books" to books, "bookForm" to form))
-            //TODO 正しくテスト出来ていない？
-//            val result =  HttpResponse.ok("aa") // これでも通ってしまう。
-            assertEquals(result, body)
-        }
+    @Inject
+    @Client("/")
+    internal var client: RxHttpClient? = null
 
-        it ("new page") {
-            val body = client.toBlocking().retrieve("/books/new")
-            val form = BookForm()
-            val result:HttpResponse<*> =  HttpResponse.ok(mapOf("bookForm" to form))
-            //TODO 正しくテスト出来ていない？
-            assertEquals(result, body)
-        }
+    @Test
+    fun testHello() {
+        val request = HttpRequest.GET<Any>("/hello")
+        val body = client!!.toBlocking().retrieve<Any>(request)
 
-        it ("edit page") {
-            val body = client.toBlocking().retrieve("/books/[0-9].*/edit")
-            //TODO 正しくテスト出来ていない？
-            assertThat(body).contains("書籍変更")
-        }
-
-        it ("no page") {
-            val body = client.toBlocking().retrieve("/hello")
-//            val body = client?.toBlocking()?.retrieve("/books")
-//            assertEquals("", body)
-            assertNotNull(body)
-        }
-
-        afterGroup {
-            client.close()
-            embeddedServer.close()
-        }
+        // NGになるはず。
+        assertNotNull(body)
+        assertEquals("Hello World", body)
     }
-})
+}
--- src/test/kotlin/book/management/repository/JdbcBookRepositoryTest.kt
+++ src/test/kotlin/book/management/repository/JdbcBookRepositoryTest.kt
@@ -8,30 +8,31 @@ import org.assertj.core.api.Assertions.assertThat
 import book.management.Book
 
 class JdbcBookRepositoryTest(@Inject var sut: JdbcBookRepository) : Spek({
-    describe("Bookリポジトリのテスト") {
+    /*
+        describe("Bookリポジトリのテスト") {
 
-        beforeEachTest {
-        }
+            beforeEachTest {
+            }
 
-        afterEachTest {
-        }
-
-        it("何も登録していない状態のときfindAllは空のリストを返す") {
-            val got = sut.findAll()
-            assertThat(got).isEmpty()
-        }
-        it("createで登録したデータをfindByで取得する") {
-            val book = sut.create("testbook", "testauthor", "testpublisher")
-            val got = sut.findById(book.id)
-            assertThat(got).isEqualTo(book)
-        }
-        it("updateで更新したデータをfindByで取得する") {
-            //TODO 正しくテスト出来ていない？
-            val book = sut.create("testbook", "testauthor", "testpublisher")
-            val bookModify = Book(book.id, "testbook2", "testauthor2", "testpublisher2")
-            sut.update(bookModify)
-            val got = sut.findById(bookModify.id)
-            assertThat(got).isEqualTo(bookModify)
-        }
+            afterEachTest {
+            }
+            it("何も登録していない状態のときfindAllは空のリストを返す") {
+                val got = sut.findAll()
+                assertThat(got).isEmpty()
+            }
+            it("createで登録したデータをfindByで取得する") {
+                val book = sut.create("testbook", "testauthor", "testpublisher")
+                val got = sut.findById(book.id)
+                assertThat(got).isEqualTo(book)
+            }
+            it("updateで更新したデータをfindByで取得する") {
+                //TODO 正しくテスト出来ていない？
+                val book = sut.create("testbook", "testauthor", "testpublisher")
+                val bookModify = Book(book.id, "testbook2", "testauthor2", "testpublisher2")
+                sut.update(bookModify)
+                val got = sut.findById(bookModify.id)
+                assertThat(got).isEqualTo(bookModify)
+            }
     }
+     */
 })
